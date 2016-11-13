a0bbdcc37fe3f0639c2c3b44c413592c80445800
Dekker3D <dekker3d@gmail.com>
2016-11-13 09:06:36 +0100

    Implemented proper priority sorting. Not quite entirely ready yet, though.

--- LibCommExt.lua
+++ LibCommExt.lua
@@ -14,11 +14,10 @@ end
 
 local LibCommExt = APkg and APkg.tPackage or {}
 
-local CommExtMessage = {}
 local CommExtChannel = {}
 
 local QueueReady = false
-local LibCommExtQueuePkg = Apollo.GetPackage("LibCommExtQueueQueue-1.0")
+local LibCommExtQueuePkg = nil
 local LibCommExtQueue = nil
 
 ---------------------------------------------------------------------------------------------------
@@ -33,21 +32,17 @@ end
 
 function LibCommExt:EnsureInit()
 	if self.Initialized ~= true then
-		self.LibPriority = 0.0 -- the most basic version right now. Any newer versions should have priority.
 		self.ChannelTable = self.ChannelTable or {}
 		self.Initialized = true
 	end
 	if self.QueueReady ~= true then
+		LibCommExtQueuePkg = Apollo.GetPackage("LibCommExtQueue-1.0")
 		if LibCommExtQueuePkg ~= nil and LibCommExtQueuePkg.tPackage ~= nil then
 			LibCommExtQueue = LibCommExtQueuePkg.tPackage
 			self.QueueReady = true
 		end
 	end
 	if self.QueueReady == true and self.Initialized == true then self.Ready = true end
-end
-
-function LibcommExt:HandleQueue(queue, remainingChars)
-	
 end
 
 function LibCommExt:GetChannel(channelName)
@@ -58,29 +53,109 @@ function LibCommExt:GetChannel(channelName)
 	return self.ChannelTable[channelName]
 end
 
-function CommExtMessage:new(o)
-	o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-	return o
-end
-
-function CommExtMessage:GetLength()
-	return
-end
-
 function CommExtChannel:new(channelName)
 	if channelName == nil or type(channelName) ~= "string" then return end
 	o = {}
 	setmetatable(o, self)
 	self.__index = self
 	o.Channel = channelName
-	o.QueueHandler = LibCommExt
+	o.Callbacks = {}
+	o:Connect()
 	return o
 end
 
 function CommExtChannel:Connect()
-	
+	if self.Channel == nil or type(self.Channel) ~= "string" or self.Channel:len() <= 0 then return end
+	if self.Comm ~= nil and self.Comm:IsReady() then
+		return
+	end
+	self.Comm = ICCommLib.JoinChannel(self.Channel, ICCommLib.CodeEnumICCommChannelType.Global)
+	if self.Comm ~= nil then
+		self.Comm:SetJoinResultFunction("OnJoinResult", self)
+		self.Comm:SetReceivedMessageFunction("OnMessageReceived", self)
+		self.Comm:SetSendMessageResultFunction("OnMessageSent", self)
+		self.Comm:SetThrottledFunction("OnMessageThrottled", self)
+	else
+		self:Print("Failed to open channel")
+	end
+end
+
+function CommExtChannel:IsReady()
+	return self.Comm ~= nil and self.Comm:IsReady()
+end
+
+function CommExtChannel:AddReceiveCallback(callback, owner)
+	table.insert(self.Callbacks, {Callback = callback, Owner = owner})
+end
+
+function CommExtChannel:OnJoinResult(channel, eResult)
+	if eResult == ICCommLib.CodeEnumICCommJoinResult.Join then
+		self:Print(string.format('Joined ICComm Channel "%s"', channel:GetName()))
+		if channel:IsReady() then
+			self:Print('Channel is ready to transmit')
+		else
+			self:Print('Channel is not ready to transmit')
+		end
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.BadName then
+		self:Print(1, 'Channel ' .. channel .. ' has a bad name.')
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.Left then
+		self:Print(1, 'Failed to join channel')
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.MissingEntitlement then
+		self:Print(1, 'Failed to join channel')
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.NoGroup then
+		self:Print(1, 'Failed to join channel')
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.NoGuild then
+		self:Print(1, 'Failed to join channel')
+	elseif eResult == ICCommLib.CodeEnumICCommJoinResult.TooManyChannels then
+		self:Print(1, "You are in too many channels to join the TIM channel")
+	else
+		self:Print(1, 'Failed to join channel; join result: ' .. eResult)
+	end
+end
+
+function CommExtChannel:OnMessageReceived(channel, strMessage, strSender)
+	for k, v in pairs(self.Callbacks) do
+		v.Callback(v.Owner, channel, strMessage, strSender)
+	end
+end
+
+function CommExtChannel:SendMessage(message, version, priority)
+	self:SendPrivateMessage(message, nil, version, priority)
+end
+
+function CommExtChannel:SendPrivateMessage(message, recipient, version, priority) -- secretly doubles as the non-private-message function.
+	if message == nil then
+		return true
+	end
+	if self.Comm == nil then
+		self:Connect()
+		return false
+	elseif not self.Comm:IsReady() then
+		self:Connect()
+		return false
+	end
+	if recipient == nil then
+		if self.Comm:SendMessage(message) then
+			self:Print(5, "Message Sent: " .. message)
+			if self.heartBeatTimer ~= nil then self.heartBeatTimer:Stop() end
+			self.heartBeatTimer = ApolloTimer.Create(60.0, true, "sendHeartbeatMessage", self)
+			return true
+		end
+	else
+		if self.Comm:SendPrivateMessage(recipient, message) then
+			self:Print(5, "Message Sent to " .. recipient .. ": " .. message)
+			return true
+		end
+	end
+	self:Print(5, "Message sending failed: " .. message)
+	return false
+end
+
+function CommExtChannel:HandleQueue(message, remainingChars)
+	if remainingChars >= message.Length then
+		return message.Length
+	end
+	return 0
 end
 
 Apollo.RegisterPackage(LibCommExt, MAJOR, MINOR, {})
--- LibCommExtQueue.lua
+++ LibCommExtQueue.lua
@@ -7,19 +7,18 @@
 General queue for LibCommExt. Separated for robustness, in case people use wildly different versions of LibCommExt.
 
 To apply as a valid library for use with this queue, you must implement a HandleQueue(queue table, maximum characters to send) function that returns the amount of characters sent.
-You will also likely want to have a LibPriority value or a GetLibPriority function to indicate when your library should get its turn.
 ]]--
  
 require "ICComm"
 require "ICCommLib"
 
-local MAJOR, MINOR = "LibCommExtQueueQueue-1.0", 1
+local MAJOR, MINOR = "LibCommExtQueue", 1
 local APkg = Apollo.GetPackage(MAJOR)
 if APkg and (APkg.nVersion or 0) >= MINOR then
 	return -- no upgrade needed
 end
 
-local LibCommExtQueueQueue = APkg and APkg.tPackage or {}
+local LibCommExtQueue = APkg and APkg.tPackage or {}
 
 ---------------------------------------------------------------------------------------------------
 -- Utility Functions
@@ -34,33 +33,38 @@ end
 function LibCommExtQueue:EnsureInit()
 	if self.Initialized == true then return end
 	self.Queue = self.Queue or {}
-	self.LibQueue = self.libQueue or {}
 	self.Initialized = true
+	self.IgnoredSenders = {}
 end
 
-function LibcommExtQueue:AddSendingLibrary(library, priority)
-	if library ~= nil and library.HandleQueue ~= nil and type(library.HandleQueue) == "function" then
-		self:EnsureInit()
-		table.insert(self.LibQueue, library)
-		table.sort(self.LibQueue, function(a, b)
-			local aPrio, bPrio = 0.0, 0.0
-			if a ~= nil and a.LibPriority ~= nil and type(a.LibPriority) == "number" then aPrio = a.LibPriority end
-			if a ~= nil and a.GetLibPriority ~= nil and type(a.GetLibPriority) == "function" then aPrio = a.GetLibPriority() end
-			if b ~= nil and b.LibPriority ~= nil and type(b.LibPriority) == "number" then bPrio = b.LibPriority end
-			if b ~= nil and b.GetLibPriority ~= nil and type(b.GetLibPriority) == "function" then bPrio = b.GetLibPriority() end
-		end )
-		return aPrio > bPrio
-	emd
+function LibCommExtQueue:CheckSendingLibrary(library)
+	if library == nil or library.HandleQueue == nil or type(library.HandleQueue) ~= "function" then return false end
+	if self:AllowedSendingLibrary(library) == false then return false end
+	return true
 end
 
-function LibCommExtQueue:AddToQueue(message)
+function LibCommExtQueue:AddToQueue(message, priority, sendingLib)
 	self:EnsureInit()
-	if message.SendingLib ~= nil then
-		table.insert(self.Queue, message)
-	end
-	if self.Timer == nil then -- Start sending immediately if we've run out of messages and had been waiting.
-		self:MessageLoop()
-		self.Timer = ApolloTimer.Create(1, true, "MessageLoop", self)
+	if self:CheckSendingLibrary(sendingLib) then
+		self.SequenceNum = (self.SequenceNum or 0) + 1
+		table.insert(self.Queue, {Message = message, Priority = priority, SendingLibrary = sendingLib, SequenceNum = self.SequenceNum})
+		
+		table.sort(self.Queue, function(a,b)
+			if a == nil and b == nil then return false end
+			if a == nil then return true end -- a should go at the end
+			if b == nil then return false end -- b should go at the end
+			if a.Priority ~= b.Priority then
+				if a.Priority == nil then return true end
+				if b.Priority == nil then return false end
+				return a.Priority > b.Priority -- higher priority goes lower in the list
+			end
+			return a.SequenceNum < b.SequenceNum
+		end)
+		
+		if self.Timer == nil then -- Start sending immediately if we've run out of messages and had been waiting.
+			self:MessageLoop()
+			self.Timer = ApolloTimer.Create(1, true, "MessageLoop", self)
+		end
 	end
 end
 
@@ -77,15 +81,16 @@ function LibCommExtQueue:MessageLoop()
 	end
 	self.CharactersSent = 0
 	self.RemainingCharacters = 90 -- safety margin. We don't want to get throttled, and some addons might use minimal amounts of traffic and not want this library.
-	for v in ipairs(self.LibQueue) do
-		self.CurrentQueueLib = v
-		pcall(HandleQueue)
+	for v in ipairs(self.Queue) do
+		self.CurrentMessage = v
+		pcall(HandleMessage)
 	end
 end
 
-function LibCommExtQueue:HandleQueue()
-	if self.CurrentQueueLib ~= nil and self.CurrentQueueLib.HandleQueue ~= nil and type(self.CurrentQueueLib.HandleQueue) == "function" then
-		local sent = self.CurrentQueueLib.HandleQueue(self.Queue, self.RemainingCharacters)
+function LibCommExtQueue:HandleMessage()
+	self:EnsureInit()
+	if self.CurrentMessage ~= nil and self:CheckSendingLibrary(self.CurrentMessage.SendingLibrary) then
+		local sent = self.CurrentMessage.SendingLibrary.HandleQueue(self.CurrentMessage.Message, self.RemainingCharacters)
 		local validResult = true
 		if sent ~= nil and type(sent) == "number" then
 			if sent > self.RemainingCharacters then validResult = false end
@@ -95,14 +100,56 @@ function LibCommExtQueue:HandleQueue()
 			validResult = false
 		end
 		if validResult == false then
-		-- remove library, it's not behaving like it should.
-			local key = nil
-			for k, v in pairs(self.LibQueue) do
-				if v == self.CurrentQueueLib then key = k end
-			end
-			if key ~= nil then table.remove(self.LibQueue, key) end
+			table.insert(self.IgnoredSenders, self.CurrentMessage.SendingLibrary)
+			self:FilterList(table, function(item) return not self:AllowedSendingLibrary(item) end)
 		end
 	end
+end
+
+function LibCommExtQueue:AllowedSendingLibrary(item)
+	if item == nil or item.SendingLibrary == nil then return false end
+	for k, v in pairs(self.FilterList) do
+		if item.SendingLibrary == v then return false end
+	end
+	return true
+end
+
+function LibCommExtQueue:RemoveFromList(table, item)
+	local key = nil
+	for k, v in pairs(table) do
+		if v == item then
+			key = k
+			break
+		end
+	end
+	if key ~= nil then
+		table.remove(table, key)
+	end
+end
+
+function LibCommExtQueue:FilterList(table, func)
+	local keysArray = {}
+	local keysTable = {}
+	for k, v in pairs(table) do
+		if func(v) then
+			if type(k) == "number" then
+				table.insert(keysArray, k)
+			else
+				table.insert(keysTable, k)
+			end
+		end
+	end
+	for k, v in pairs(keysTable) do
+		table[v] = nil
+	end
+	table.sort(keysArray, function(a,b) return a > b end)
+	for v in ipairs(keysArray) do
+		table.remove(table, v) -- remove in reverse order.
+	end
+end
+
+function LibCommExtQueue:RemoveMessageFromQueue(message)
+	self:RemoveFromList(self.Queue, message)
 end
 
 Apollo.RegisterPackage(LibCommExtQueue, MAJOR, MINOR, {})
